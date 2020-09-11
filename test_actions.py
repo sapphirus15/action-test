5abb19ef3664777541ccd870fabfae96b22903fc
sungmin <smyoon@sungmins-MacBook-Pro.local>
2020-09-11 15:20:43 +0900

    action update

--- test_actions.py
+++ test_actions.py
@@ -1,9 +1,4 @@
 def test_ci_test():
-    assert 1 == 1, "반드시 같아야함."
+    assert 1 == 1, "반드시 같아야죠 ~ "
 
-def test_simple_another_test():
-    assert 1
-
-def test_simple_another_test_fail():
-    assert 0
-
+    
