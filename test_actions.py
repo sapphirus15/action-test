b3a6341a46a96bd2f3a598d0a7aa63c88977b6e5
sungmin <smyoon@sungmins-MacBook-Pro.local>
2020-09-11 15:14:04 +0900

    actions push test

--- test_actions.py
+++ test_actions.py
@@ -1,2 +1,9 @@
 def test_ci_test():
-    assert 1 == 1, "반드시 같아야죠 ~ "
+    assert 1 == 1, "반드시 같아야함."
+
+def test_simple_another_test():
+    assert 1
+
+def test_simple_another_test_fail():
+    assert 0
+
